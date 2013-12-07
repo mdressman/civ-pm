c2997f54948df2b89ec230c19e406f8e46011603
Matthew Dressman <md@mdressman.com>
2013-12-06 17:30:44 -0800

    implement Branding 1 milestone template and beginnings of week deadline views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -58,18 +58,59 @@ class ProjectsController < ApplicationController
 
         def create_milestones
             case @project.project_type
-                when "Print Design"
-                    Milestone.create("name" => "Internal Review", "estimated_date" => set_estimated_deadline(3), "estimated_time" => 8, "client_id" => @client.id, "project_id" => @project.id)
-                when "Interface Design"
-                    Milestone.create("name" => "Internal Review", "estimated_date" => set_estimated_deadline(3), "estimated_time" => 8, "client_id" => @client.id, "project_id" => @project.id)
-                when "Web Development"
-                    Milestone.create("name" => "Internal Review", "estimated_date" => set_estimated_deadline(3), "estimated_time" => 8, "client_id" => @client.id, "project_id" => @project.id)
+                when "Branding 1"
+                    branding1_milestones = [
+                                                ['Kickoff', 1, 3],
+                                                ['Branding Exercise', 2, 2],
+                                                ['Mood Board 1 Design', 3, 4],
+                                                ['Mood Board 1 Internal Review', 3, 1],
+                                                ['Mood Board 1 Refinement', 4, 2],
+                                                ['Mood Board 1 Presentation', 4, 2],
+                                                ['Sketches', 5, 20],
+                                                ['Sketches Internal Review', 5, 2],
+                                                ['Mood Board 2 Design', 5, 4],
+                                                ['Mood Board 2 Internal Review', 6, 2],
+                                                ['Mood Board 2 Refinement', 6, 2],
+                                                ['Mood Board 2 Presentation', 6, 2],
+                                                ['Logo Round 1 Design', 6, 20],
+                                                ['Logo Round 1 Internal Review', 7, 2],
+                                                ['Logo Round 1 Refinement', 8, 2],
+                                                ['Logo Round 1 Presentation', 8, 2],
+                                                ['Logo Round 2 Design', 9, 16],
+                                                ['Logo Round 2 Internal Review', 10, 2],
+                                                ['Logo Round 2 Refinement', 10, 2],
+                                                ['Logo Round 2 Presentation', 10, 2],
+                                                ['Send Final Logo Package', 11, 8],
+                                                ['Style Guide Development', 11, 16],
+                                                ['Style Guide Internal Review', 12, 2],
+                                                ['Style Guide Refinement', 12, 2],
+                                                ['Send Style Guide', 12, 2]
+                                        ]
+                    branding1_milestones.each do |ms| 
+                        Milestone.create("name" => ms[0], "estimated_date" => set_estimated_deadline(ms[1]), "estimated_time" => ms[2], "client_id" => @client.id, "project_id" => @project.id)
+                    end
+                    # Milestone.create("name" => "Internal Review", "estimated_date" => set_estimated_deadline(3), "estimated_time" => 8, "client_id" => @client.id, "project_id" => @project.id)
+
+                when "Branding 2"
+                
+                when "Print 1"
+
+                when "Print 2"
+
+                when "Web Design 1"
+
+                when "Web Design 2"
+                
+                when "Web Development 1"
+
+                when "Web Development 2"    
+                
             end
         end
 
         def set_estimated_deadline(distance)
-            if @project.end_date
-                @project.end_date - distance.days
+            if @project.start_date
+                @project.start_date + distance.weeks
             else
                 nil
             end
